473649c169d428974c235b9b634b66ca2c0e93d3
Shubh Batra <shubhbatra12@gmail.com>
2020-03-30 13:05:25 +0530

    First Commit

---
+++ app/src/main/java/com/example/todonew/TaskActivity.kt
@@ -0,0 +1,176 @@
+package com.example.todonew
+
+import android.app.AlertDialog
+import android.app.DatePickerDialog
+import android.app.Dialog
+import android.app.TimePickerDialog
+import android.content.Intent
+import android.content.res.Resources
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.DatePicker
+import android.widget.TimePicker
+import android.widget.Toast
+import android.widget.Toast.LENGTH_SHORT
+import androidx.room.Room
+import kotlinx.android.synthetic.main.activity_task.*
+import kotlinx.android.synthetic.main.addcat_dialog.view.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
+
+const val DB_NAME = "todo.db"
+class TaskActivity : AppCompatActivity(), View.OnClickListener {
+
+    lateinit var myCalendar: Calendar
+    lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
+    lateinit var timeSetListener: TimePickerDialog.OnTimeSetListener
+
+    var finalDate = 0L
+    var finalTime = 0L
+
+    private val labels = arrayListOf(
+        "Personal", "Business", "Insurance", "Shopping", "Banking"
+    )
+
+    val db by lazy {
+        AppDatabase.getDatabase(this)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task)
+
+        dateEdt.setOnClickListener(this)
+        timeEdt.setOnClickListener(this)
+        saveBtn.setOnClickListener(this)
+        imgAddCategory.setOnClickListener(this)
+
+        setUpSpinner()
+    }
+
+    private fun setUpSpinner() {
+        val adapter = ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,labels)
+
+        labels.sort()
+        spinnerCategory.adapter = adapter
+    }
+
+    override fun onClick(v: View) {
+        when(v.id){
+            R.id.dateEdt -> {
+                setListener()
+            }
+            R.id.timeEdt -> {
+                setTimeListener()
+            }
+            R.id.saveBtn -> {
+                saveTodo()
+            }
+            R.id.imgAddCategory -> {
+
+                openDialog(labels)
+            }
+        }
+    }
+
+    private fun openDialog(list : ArrayList<String>) {
+        val mDialogView = LayoutInflater.from(this).inflate(R.layout.addcat_dialog, null)
+        val mBuilder = AlertDialog.Builder(this)
+            .setView(mDialogView)
+            .setTitle("Category Add Dialog")
+        val  mAlertDialog = mBuilder.show()
+
+        mDialogView.dialogSaveBtn.setOnClickListener {
+            mAlertDialog.dismiss()
+            val name = mDialogView.addCatText.text.toString()
+            list.add(name)
+        }
+        mDialogView.dialogCancelBtn.setOnClickListener {
+            mAlertDialog.dismiss()
+        }
+
+    }
+
+
+
+    private fun saveTodo() {
+        val category = spinnerCategory.selectedItem.toString()
+        val title = titleInpLay.editText?.text.toString()
+        val description = taskInpLay.editText?.text.toString()
+
+        GlobalScope.launch(Dispatchers.Main) {
+            val id = withContext(Dispatchers.IO) {
+                return@withContext db.todoDao().insertTask(
+                    TodoModel(
+                        title,
+                        description,
+                        category,
+                        finalDate,
+                        finalTime
+                    )
+                )
+            }
+            finish()
+        }
+    }
+
+    private fun setTimeListener() {
+        myCalendar = Calendar.getInstance()
+
+        timeSetListener = TimePickerDialog.OnTimeSetListener { _: TimePicker, hourOfDay : Int , min :Int ->
+            myCalendar.set(Calendar.HOUR_OF_DAY, hourOfDay)
+            myCalendar.set(Calendar.MINUTE, min)
+
+            updateTime()
+        }
+        val timePickerDialog = TimePickerDialog(
+            this,timeSetListener,myCalendar.get(Calendar.HOUR_OF_DAY),
+            myCalendar.get(Calendar.MINUTE),false
+        )
+        timePickerDialog.show()
+    }
+
+    private fun updateTime() {
+        val myFormat = "h:mm a"
+        val sdf = SimpleDateFormat(myFormat)  // it doesnt have to be converted into time ?
+        finalTime = myCalendar.time.time
+        timeEdt.setText(sdf.format(myCalendar.time))
+
+    }
+
+    private fun setListener() {
+        myCalendar = Calendar.getInstance()
+
+        dateSetListener = DatePickerDialog.OnDateSetListener { _: DatePicker, year: Int, month: Int, dayOfMonth: Int ->
+            myCalendar.set(Calendar.YEAR, year)
+            myCalendar.set(Calendar.MONTH, month)
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+
+            updateDate()
+        }
+        val datePickerDialog = DatePickerDialog(
+            this,dateSetListener,myCalendar.get(Calendar.YEAR),
+            myCalendar.get(Calendar.MONTH),myCalendar.get(Calendar.DAY_OF_MONTH)
+        )
+        datePickerDialog.datePicker.minDate = System.currentTimeMillis()
+        datePickerDialog.show()
+    }
+
+    private fun updateDate() {
+        val myFormat = "EEE, d MMM YYYY"
+        val sdf = SimpleDateFormat(myFormat)
+        finalDate = myCalendar.time.time
+        dateEdt.setText(sdf.format(myCalendar.time))
+
+        timeInptlay.visibility= View.VISIBLE
+    }
+
+}
---
+++ app/src/main/java/com/example/todonew/TodoDao.kt
@@ -0,0 +1,21 @@
+package com.example.todonew
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface TodoDao {
+    @Insert()
+    suspend fun insertTask(todoModel: TodoModel):Long
+
+    @Query("Select * from TodoModel where isFinished ==  0")
+    fun getTask():LiveData<List<TodoModel>>
+
+    @Query("Update TodoModel set isFinished = 1 where id=:uid")
+    fun finishTask(uid:Long)
+
+    @Query("Delete from TodoModel where id=:uid")
+    fun deleteTask(uid:Long)
+}
